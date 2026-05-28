21a0783a690120f5ae6e2a27d06f3a702a3aaa4d
Dhyanesh-spec <dhyaneshdhyanesh60@gmail.com>
2026-05-28 21:19:26 +0530

    Add menu and play

---
+++ menu.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+func _on_play_button_pressed() -> void:
+	# This exits the menu and opens your top-down grass map!
+	get_tree().change_scene_to_file("res://main.tscn")
+
+func _on_quit_button_pressed() -> void:
+	# This closes Variant Zero cleanly when quit is clicked
+	get_tree().quit()
+
+func _on_credit_button_pressed() -> void:
+	# This prints a message to your debugger console at the bottom
+	print("Variant Zero - Developed by Shalom and Dhyanesh!")
